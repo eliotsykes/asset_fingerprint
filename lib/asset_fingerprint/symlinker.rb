e03a4a1c00806a09affa0a01ff982e7b8ff8f940
Eliot Sykes <eliotsykes@gmail.com>
2010-04-27 17:05:11 +0100

    Added symlink_on_the_fly option

--- lib/asset_fingerprint/symlinker.rb
+++ lib/asset_fingerprint/symlinker.rb
@@ -1,15 +1,23 @@
 module AssetFingerprint
+  
+  @@symlink_on_the_fly = true
+  def self.symlink_on_the_fly=(value)
+    @@symlink_on_the_fly = value
+  end
+  
+  def self.symlink_on_the_fly?
+    @@symlink_on_the_fly
+  end
+  
   class Symlinker
     
     @@symlinked = []
     
     def self.execute(source, fingerprinted_path)
-      if already_symlinked?(fingerprinted_path)
-        Rails.logger.info("'#{fingerprinted_path}' is already symlinked")
-      else
+      return unless enabled?
+      unless already_symlinked?(fingerprinted_path)
         abs_source_path = abs_path_to_asset(source)
         abs_fingerprinted_path  = abs_path_to_asset(fingerprinted_path)
-        Rails.logger.info("Creating symlink '#{abs_fingerprinted_path}'")
         FileUtils.ln_sf(abs_source_path, abs_fingerprinted_path)
         @@symlinked << fingerprinted_path
       end
@@ -21,6 +29,10 @@ module AssetFingerprint
     
     def self.abs_path_to_asset(source)
       ActionView::Helpers::AssetTagHelper.abs_path_to_asset(source)
+    end
+    
+    def self.enabled?
+      AssetFingerprint.symlink_on_the_fly?
     end
     
   end
