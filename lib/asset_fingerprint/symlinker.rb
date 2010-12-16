ef8c0b0c3cdefcba309756f647fc7c2b559aaa46
Evan Worley <eworley@genevan.(none)>
2010-12-15 18:06:47 -0800

    o Converting cache from array to set for performance reasons o Making missing parent directories when generating links

--- lib/asset_fingerprint/symlinker.rb
+++ lib/asset_fingerprint/symlinker.rb
@@ -19,7 +19,7 @@ module AssetFingerprint
   
   class Symlinker
     
-    @@symlinked = []
+    @@symlinked = Set.new
     
     def self.symlink_on_the_fly(asset)
       return unless AssetFingerprint.symlink_on_the_fly?
@@ -28,13 +28,21 @@ module AssetFingerprint
     
     def self.execute(asset)
       if asset.symlinkable? && !already_symlinked?(asset.fingerprinted_path)
+
+        # Make any missing parent directories
+        FileUtils.mkdir_p(File.dirname(asset.fingerprinted_absolute_path))
+
         FileUtils.ln_sf(asset.source_absolute_path, asset.fingerprinted_absolute_path)
         @@symlinked << asset.fingerprinted_path
       end
     end
     
     def self.already_symlinked?(fingerprinted_path)
-      @@symlinked.include?(fingerprinted_path) || File.exists?(fingerprinted_path)
+      if AssetFingerprint.symlink_on_the_fly?  
+        File.exists?(fingerprinted_path)
+      else
+        @@symlinked.include?(fingerprinted_path) 
+      end
     end
     
   end
